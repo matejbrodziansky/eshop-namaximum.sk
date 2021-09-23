32ddb1d1244b465db9ffa786dd36271f73f17d5c
matejbrodziansky <matejbrodziansky@gmail.com>
2021-09-23 10:37:28 +0200

    AJAX CREATING (add-category.php)

--- application/views/admin/add-category.php
+++ application/views/admin/add-category.php
@@ -1,6 +1,26 @@
+<style>
+    .modal {
+        display: none;
+        position: fixed;
+        z-index: 1000;
+        top: 0;
+        left: 0;
+        height: 100%;
+        width: 100%;
+        background: rgba(255, 255, 255, .8) url('http://i.stack.imgur.com/FhHRx.gif') 50% 50% no-repeat;
+    }
+
+    body.loading .modal {
+        overflow: hidden;
+    }
+
+    body.loading .modal {
+        display: block;
+    }
+</style>
 
 <div style="margin-left: 2em;" class=" mt-4">
-    <a  class="btn btn-primary" href="<?= base_url('admin/showcategories') ?>">Show</a>
+    <a class="btn btn-primary" href="<?= base_url('admin/showcategories') ?>">Show</a>
 </div>
 
 
@@ -11,11 +31,11 @@
         <div class="row">
             <div class="col">
                 <ul>
-                    <li>DOPLNKY VÝŽIVY            ID = 1</li>
-                    <li>ZDRAVÉ POTRAVINY          ID = 2</li>
+                    <li>DOPLNKY VÝŽIVY ID = 1</li>
+                    <li>ZDRAVÉ POTRAVINY ID = 2</li>
                     <li>OBLEČENIE A PRÍSLUŠENSTVO ID = 3</li>
-                    <li>EKO DROGÉRIA              ID = 4</li>
-                    <li>TVOJE CIELE               ID = 5</li>
+                    <li>EKO DROGÉRIA ID = 4</li>
+                    <li>TVOJE CIELE ID = 5</li>
                 </ul>
 
             </div>
@@ -29,24 +49,24 @@
         </div>
     </div>
     <form method="post">
-                    
+
         <div class="form-group">
             <label for="exampleFormControlInput1">Navigation Id</label>
-            <input type="text" class="form-control" name="navigation_id" id="exampleFormControlInput1" >
+            <input type="text" class="form-control" name="navigation_id" id="exampleFormControlInput1">
         </div>
         <div class="form-group">
             <label for="exampleFormControlInput1">Name</label>
-            <input type="text" class="form-control" name="name" id="exampleFormControlInput1" >
+            <input type="text" class="form-control" name="name" id="exampleFormControlInput1">
         </div>
         <div class="form-group">
             <label for="exampleFormControlInput1">Slug</label>
-            <input type="text" class="form-control" name="slug" id="exampleFormControlInput1" >
+            <input type="text" class="form-control" name="slug" id="exampleFormControlInput1">
         </div>
         <div class="form-group">
             <label for="exampleFormControlInput1">Parent id</label>
-            <input type="text" class="form-control" name="parent_id" id="exampleFormControlInput1" >
+            <input type="text" class="form-control" name="parent_id" id="exampleFormControlInput1">
         </div>
-        <input type="hidden" class="form-control" name="category" id="exampleFormControlInput1" value="category" >
+        <input type="hidden" class="form-control" name="category" id="exampleFormControlInput1" value="category">
 
         <input type="submit" class="btn btn-success" value="Uložiť" id="formConfirm">
 
@@ -59,27 +79,65 @@
     <form method="post">
         <div class="form-group">
             <label for="exampleFormControlInput1">Parent id</label>
-            <input type="text" class="form-control" name="parent_id" id="exampleFormControlInput1" >
+            <input type="text" class="form-control" name="parent_id" id="exampleFormControlInput1">
         </div>
         <div class="form-group">
             <label for="exampleFormControlInput1">Name</label>
-            <input type="text" class="form-control" name="name" id="exampleFormControlInput1" >
+            <input type="text" class="form-control" name="name" id="exampleFormControlInput1">
         </div>
         <div class="form-group">
             <label for="exampleFormControlInput1">Slug</label>
-            <input type="text" class="form-control" name="slug" id="exampleFormControlInput1" >
+            <input type="text" class="form-control" name="slug" id="exampleFormControlInput1">
         </div>
-        
+
         <!-- <div class="form-group">
             <label for="exampleFormControlFile1">Photo of product</label>
             <input type="file" class="form-control-file" id="exampleFormControlFile1">
         </div> -->
 
-        <input type="hidden" class="form-control" name="category" id="exampleFormControlInput1" value="subcategory" >
+        <input type="hidden" class="form-control" name="category" id="exampleFormControlInput1" value="subcategory">
 
         <input type="submit" class="btn btn-success" value="Uložiť" id="formConfirm">
     </form>
 </div>
+
+<div class="modal"></div>
+
+
+<script>
+    $('form').submit(function(e) {
+
+        var createUrl = '<?= base_url('admin/create') ?>',
+            body = $("body");
+
+        e.preventDefault();
+        $(document).on({
+            ajaxStart: function() {
+                body.addClass("loading").css({
+                    backgroundColor: 'gray'
+                });
+            },
+            ajaxStop: function() {
+                body.removeClass("loading").css({
+                    backgroundColor: '#F4F3F3'
+                });
+
+            }
+        });
+
+        $.ajax({
+            url: createUrl,
+            type: 'POST',
+            data: new FormData(this),
+            processData: false,
+            contentType: false,
+            success: function(data) {
+            },
+            error: function(xhr, ajaxOptions, thrownerror) {}
+        });
+        e.preventDefault();
+    });
+</script>
 
 </body>
 
